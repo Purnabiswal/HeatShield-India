5bd09779fad2ff38a1b522f72e083a285bcf60cc
Purnabiswal <purnabiswal824@gmail.com>
2026-07-02 08:11:02 +0530

    Backend & Frontend Fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://heatshield-india.onrender.com";
 
 export async function calculateSolar(data) {
   const response = await fetch(`${BASE_URL}/planner/calculate`, {
